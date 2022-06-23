7cb013e77aa76030f7b3b89fca96b6e99d47e62a
autodoc <alexeym@pkwteile.de>
2022-06-23 16:30:51 +0300

    commit

--- src/test/java/UI/UiFailExampleTest.java
+++ src/test/java/UI/UiFailExampleTest.java
@@ -1,6 +1,7 @@
 package UI;
 
 import SetUp.SetUpBrowser;
+import io.qameta.allure.*;
 import org.testng.annotations.Test;
 
 public class UiFailExampleTest extends SetUpBrowser {
@@ -9,6 +10,16 @@ public class UiFailExampleTest extends SetUpBrowser {
     private final String MONTH = "Septemer 2022";
 
     @Test
+    @Owner("Daniel")
+    @Lead("Paul Fowler")
+    @Description("Test check correct page after choosing Flexible Dates")
+    @Severity(SeverityLevel.NORMAL)
+    @Epic("Checking availability")
+    @Feature("Check In - Check Out")
+    @Story("User should choose Flexible Dates")
+    @Issue("WW-2")
+    @TmsLink("tc-2")
+    @Link(name = "requirements", url="https://www.somerequire.com",type = "Trello")
     public void UiFailTest() {
         openPage(URL);
         new MainPageLogic()
